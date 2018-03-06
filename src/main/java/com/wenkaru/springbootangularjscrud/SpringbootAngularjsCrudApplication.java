a8a33f021e427350d2787c99ffa8da2c816269a2
Danilo Madrigalejos <danilo.madrigalejos@amdatex.com>
2018-03-06 13:52:34 +0800

    Initial commit

---
+++ src/main/java/com/wenkaru/springbootangularjscrud/SpringbootAngularjsCrudApplication.java
@@ -0,0 +1,12 @@
+package com.wenkaru.springbootangularjscrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAngularjsCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAngularjsCrudApplication.class, args);
+	}
+}
